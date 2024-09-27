e71064292361e7ce7d32f1ad49863440ef7ebbfe
CarlosFerreiraCPNV <150017797+CarlosFerreiraCPNV@users.noreply.github.com>
2024-09-27 14:07:51 +0200

    Update Commentaires

--- Run, Riley, Run !/main.py
+++ Run, Riley, Run !/main.py
@@ -1,8 +1,8 @@
 """
 Auteur : Carlos Ferreira & Théo Nussbaum
-Date : 27.09.2024
+Date : 06.09.2024
 Projet : Run, Riley, Run!
-Description : Fichier du menu start et du jeu
+Description : Fichier du système du jeu
 """
 import pygame
 import random
@@ -13,11 +13,7 @@ pygame.init()
 # Variables Constantes
 screen_width = 1920
 screen_height = 1080
-
-game_speed = 10
-
 fps = 60
-
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 CITY = pygame.image.load(r".\background\CITY.png").convert_alpha()
@@ -62,37 +58,43 @@ start_pressed_image = pygame.image.load(r".\images\start_button_pressed.png")
 rules_unpressed_image = pygame.image.load(r".\images\rules_button_unpressed.png")
 rules_pressed_image = pygame.image.load(r".\images\rules_button_pressed.png")
 
-Run_Menu = True
-menu_close = False
 start_button = Button(662, 489, start_unpressed_image)
 rules_button = Button(782, 803, rules_unpressed_image)
 
+# Variables
+game_speed = 10
+Run_Menu = True
+menu_close = False
 
-# Class du joueur
+#######################################################################################################################
+############################################### Classe du joueur ######################################################
+#######################################################################################################################
+
 class Player:
     # Position du joueur par défaut
     x_pos = 40  # jumping = 83  / running = 40
     y_pos = 795 # jumping = 620 / running = 795
 
-
+    # Déclarations des variables d'initialisation du joueur
     def __init__(self):
-        # Déclarations de variables pour le lancement de la class Player
+        # Images des différents états du joueur
         self.bending_down_img = bending_down
         self.run_img = running
         self.jump_img = jumping
 
+        # Dans quel état est le joueur
         self.player_bending_down = False
         self.player_run = True
         self.player_jump = False
 
-        self.HOLD_STATE = False
-
+        # Positionnement du joueur et l'image affiché
         self.step_index = 0
         self.image = self.run_img[0]
         self.player_rect = self.image.get_rect()
         self.player_rect.x = self.x_pos
         self.player_rect.y = self.y_pos
 
+        # Variables pour les parametres du saut
         self.gravity = 1
         self.jump_height = 25
         self.velocity = self.jump_height
@@ -101,11 +103,12 @@ class Player:
 
     # Cette fonction servira à mettre à jour l'état et l'image du personnage en fonction de ce qu'il fait dans le jeu.
     def update(self, user_input):
-
         if self.player_bending_down:
             self.bending_down()
+
         if self.player_run:
             self.run()
+
         if self.player_jump:
             self.jump()
 
@@ -115,11 +118,9 @@ class Player:
 
         # Si le joueur appuye pour sauter et que le joueur n'est pas dans les aires (n'a pas sauté), on vas donc dire que le joueur peut sauter
         if user_input[pygame.K_UP] and not self.player_jump:
-                print(self.player_rect.x)
                 self.player_bending_down = False
                 self.player_run = False
                 self.player_jump = True
-
 
         # Si le joueur appuye pour se baisser et que le joueur n'est pas dans les aires (n'a pas sauté), on vas donc dire que le joueur peut se baisser
         elif user_input[pygame.K_DOWN] and not self.player_jump:
@@ -144,9 +145,8 @@ class Player:
 
     # Fonction pour courrir
     def run(self):
-        self.player_rect.x = 40  # jumping = 83  / running = 40
-        self.player_rect.y = 795  # jumping = 620 / running = 795"""
-        # Parametre la vitesse du personnage
+        self.player_rect.x = 40
+        self.player_rect.y = 795
         self.image = self.run_img[self.step_index // 8]
         self.player_rect = self.image.get_rect()
         self.player_rect.x = self.x_pos
@@ -159,6 +159,7 @@ class Player:
         self.player_rect.y -= self.velocity
         self.velocity -= self.gravity
         self.step_index += 1
+
         if self.velocity < - self.jump_height:
             self.player_jump = False
             self.velocity = self.jump_height
@@ -168,43 +169,66 @@ class Player:
     def draw(self, screen_parameter):
         screen_parameter.blit(self.image, (self.player_rect.x, self.player_rect.y))
 
+#######################################################################################################################
+################################### Classe qui permet de créer des obstacles ##########################################
+#######################################################################################################################
 
 class Obstacle:
     global player_alive
+
+    # Déclarations des variables d'initialisation de l'obstacle
     def __init__(self, image, type):
         self.image = image
         self.type = type
         self.rect = self.image[self.type].get_rect()
         self.rect.x = screen_width
 
+    # Permet de mettre à jour l'emplacement de l'objet qui bouge
     def update(self):
         self.rect.x -= game_speed
+
+        # Supprime l'obstacle s'il est plus sur l'écran
         if self.rect.x < -self.rect.width:
             obstacles.pop()
 
+    # Affiche l'obstacle sur l'écran
     def draw(self, screen):
         screen.blit(self.image[self.type], self.rect)
 
+#######################################################################################################################
+####################################### Classes appartenant aux obstacles #############################################
+#######################################################################################################################
+
+# Classe de l'objet Caisse
 class Box(Obstacle):
+    # Choisi le type de la caisse aléatoirement, lui attribue une image et une position
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 880
 
+
+# Classe de l'objet Poubelle
 class Trash(Obstacle):
+    # Choisi le type de la poubelle, lui attribue une image et une position
     def __init__(self, image):
         self.type = 0
         super().__init__(image, self.type)
         self.rect.y = 830
 
+
+# Classe de l'objet Balle
 class Bullet(Obstacle):
+    # Choisi le type de la balle, lui attribue une image et une position
     def __init__(self, image):
         self.type = 1
         super().__init__(image, self.type)
         self.rect.y = 780
 
+#######################################################################################################################
+########################################## Fonction de lancement du jeu ###############################################
+#######################################################################################################################
 
-# Le jeu
 def Run_Riley_Run():
     global obstacles
     game_on = True
@@ -253,6 +277,7 @@ def Run_Riley_Run():
             if FRONT_bg_x <= -1850:
                 FRONT_bg_x = 0
 
+        # S'il n'y a aucun obstacle dans la liste ajouter un obstacle aléatoirement
         if len(obstacles) == 0:
             if random.randint(0, 2) == 0:
                 obstacles.append(Box(Box_Obstacles))
@@ -261,6 +286,7 @@ def Run_Riley_Run():
             elif random.randint(0, 2) == 2:
                 obstacles.append(Trash(obstacles_objects))
 
+        # Affiche les obstacles sur l'écran et si le joueur est au même endroit que l'obstacle afficher la heatbox en rouge
         for obstacle in obstacles:
             obstacle.draw(screen)
             obstacle.update()
@@ -281,6 +307,9 @@ def Run_Riley_Run():
     # QUIT
     pygame.quit()
 
+#######################################################################################################################
+############################################# Lancement du Menu Start #################################################
+#######################################################################################################################
 
 while Run_Menu:
     timer = pygame.time.Clock()
@@ -289,12 +318,15 @@ while Run_Menu:
         if event.type == pygame.QUIT:
             Run_Menu = False
 
+    # Récupere la position de la souris
     mouse_pos = pygame.mouse.get_pos()
 
+    # Ferme le jeu si on appuye sur Q
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_q]:
         Run_Menu = False
 
+    # Affichage des images
     screen.blit(menu_bg_image, (0, 0))
     screen.blit(title_image, (11, 52))
     state = start_button.draw(screen)
@@ -311,14 +343,17 @@ while Run_Menu:
         start_button = Button(665, 504, start_pressed_image)
         menu_close = True
 
+    # On change l'image du bouton
     if rules_button.clicked:
         rules_button = Button(782, 803, rules_pressed_image)
     else:
         rules_button = Button(782, 803, rules_unpressed_image)
 
+    # Mets à jour l'affichage
     pygame.display.flip()
 
-    timer.tick(60)
+    # FPS
+    timer.tick(fps)
 
 
 # Lancement du jeu
