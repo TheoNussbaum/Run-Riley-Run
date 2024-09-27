b7624892f60698918a7581596b30b203f924790d
TheoNussbaum <150017600+TheoNussbaum@users.noreply.github.com>
2024-09-27 15:04:22 +0200

    Update Desc

--- Run, Riley, Run !/main.py
+++ Run, Riley, Run !/main.py
@@ -2,7 +2,7 @@
 Auteur : Carlos Ferreira & Théo Nussbaum
 Date : 27.09.2024
 Projet : Run, Riley, Run!
-Description : Fichier du système du jeu
+Description : Fichier du système du jeu.
 """
 import pygame
 import random
